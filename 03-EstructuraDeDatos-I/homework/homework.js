f4d9ec6939a824372de50081237c595fe74d1217
Crachelz <crzamora@gmail.com>
2022-04-28 14:50:08 -0300

    4th hw

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -7,6 +7,14 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+
+  if(n > 0 && n < 2){
+    return n
+  } else if (n < 0) {
+    return 'N no puede ser un numero negativo'
+  } else {
+    return n * nFactorial(n - 1) 
+  }
 }
 
 function nFibonacci(n) {
@@ -16,6 +24,13 @@ function nFibonacci(n) {
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
 
+  if( n <= 0 )return 0;
+  else if (n === 1 )return 1;
+  else {
+   return nFibonacci(n-1) + nFibonacci(n- 2)
+   
+ }
+
 }
 
 // Para esta parte no es necesario utilizar recursión.
@@ -24,9 +39,23 @@ function nFibonacci(n) {
 // dequeue: Remueve un valor de la queue. Obedece a FIFO y respeta el underflow (devuelve undefined cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
 // size: Devuelve el número de elementos que contiene la queue.
 
-function Queue() {
+function Queue()  {
 
+  this.arr = [];
 }
+
+Queue.prototype.enqueue = function(el){
+  this.arr.push(el);
+}
+
+Queue.prototype.dequeue = function(){
+  return this.arr.shift();
+}
+
+Queue.prototype.size = function(){
+  return this.arr.length;
+}
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
