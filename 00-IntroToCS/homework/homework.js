cf3557c7e7581af9362a2186b0d22688a9b8af12
Crachelz <crzamora@gmail.com>
2022-04-28 18:13:57 -0300

    uploading 1st hw again

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,16 +3,35 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  var result = 0
+  for (var i = 0; i < num.length; i++) {
+   result = result + num[i] * 2 ** (num.length -1 -i)
+  }
+  console.log(result)
+  return result
+  
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+
+  var binary = ''
+  while (num > 0) {
+    
+    binary = (num % 2) + binary
+    num = Math.floor(num / 2) 
+    
+  }
+ 
+  return binary
+
 }
 
 // No se pueden usar: 
 // parseInt
 // toString
 
+console.log()
 
 
 module.exports = {
